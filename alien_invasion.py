2c2f9d2a0aef60f066e87651def2053e257c359d
Meow913 <tatyanko@bk.ru>
2024-02-13 20:03:14 +0300

    13-02-2024

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,32 +2,67 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
 
     def __init__(self):
         """Инициализирует игру и создаёт игровые ресурсы"""
         pygame.init()
-        pygame.display.set_caption("Alien Invasion")
-        # Назначение цвета фона
-        self.bg_color = (230, 230, 230)
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.bg_color)
-            
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+           
+    def _check_events(self):
+        """Отрабатывает нажатия клавиш и события мыши."""
+        # Отслеживание событий клавиатуры и мыши.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)    
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)    
+
+    def _check_keydown_events(self, event):
+        """Реагирует на нажатие клавиш."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Реагирует на отпускание клавиш."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+         # При каждом проходе цикла перерисовывается экран.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
+
+
+
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
